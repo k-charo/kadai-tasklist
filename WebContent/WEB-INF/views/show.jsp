a7544a79477b3f8fe944b76788130484ee17d164
shunsuke.kobayashi <kitty_my_love2006@yahoo.co.jp>
2021-08-22 18:39:28 +0900

    フラッシュメッセージを追加

---
+++ WebContent/WEB-INF/views/show.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+
+<c:import url="../layout/app.jsp">
+    <c:param name="main">
+        <c:choose>
+            <c:when test="${task != null}">
+                <h2>id：${task.id}のタスク詳細ページ</h2>
+
+                <table>
+                    <tr>
+                        <th>タスク内容：</th>
+                        <td><c:out value="${task.content}" /></td>
+                    </tr>
+                    <tr>
+                        <th>作成日時：</th>
+                        <td><c:out value="${task.created_at}" /></td>
+                    </tr>
+                    <tr>
+                        <th>更新日時：</th>
+                        <td><c:out value="${task.updated_at}" /></td>
+                    </tr>
+                </table>
+
+                <p><a href="${pageContext.request.contextPath}/index">一覧に戻る</a></p>
+                <p><a href="${pageContext.request.contextPath}/edit?id=${task.id}">このタスクを編集する</a></p>
+            </c:when>
+            <c:otherwise>
+                <h2>お探しのデータは見つかりませんでした。</h2>
+            </c:otherwise>
+        </c:choose>
+    </c:param>
+</c:import>
--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,16 +14,16 @@ import models.Tasklist;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public ShowServlet() {
         super();
     }
 
@@ -32,22 +31,18 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Tasklist> tasks = em.createNamedQuery("getAllTasks", Tasklist.class).getResultList();
+        //該当のIDのタスク1件のみをデータベースから取得
+        Tasklist t = em.find(Tasklist.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        request.setAttribute("tasks", tasks);
-        
-        // フラッシュメッセージがセッションスコープにセットされていたら
-        // リクエストスコープに保存する（セッションスコープからは削除）
-        if (request.getSession().getAttribute("flush") != null) {
-            request.setAttribute("flush", request.getSession().getAttribute("flush"));
-            request.getSession().removeAttribute("flush");
-        }
+        //タスクデータをリクエストスコープにセットしてshow.jspを呼び出す。
+        request.setAttribute("task", t);
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/show.jsp");
         rd.forward(request, response);
     }
 
